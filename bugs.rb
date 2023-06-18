693e0f6ee683e3f46688eb2250b10bf73eef5971
Doug Turnbull <softwaredoug@gmail.com>
2023-06-18 07:35:38 -0400

    Add a homebrew recipe

---
+++ bugs.rb
@@ -0,0 +1,19 @@
+class Bugs < Formula
+  desc "Opinionated tool for doing project management in Jira at Command Line"
+  version "0.1.0"
+  homepage "https://github.com/reddit/bugs"
+  url "https://github.com/reddit/bugs.git", :tag => "v1.0.0"
+  sha256 "0123456789abcdef0123456789abcdef0123456789abcdef0123456789abcdef"
+
+  depends_on "ankitpokhrel/jira-cli/jira-cli"
+
+  def install
+    mv "bugs.sh", "bugs" # Rename the script during installation
+    bin.install "bugs"
+  end
+
+  test do
+    output = shell_output("#{bin}/bugs bunny")
+    assert_match "Neeaah, Whats up Doc", output
+  end
+end
